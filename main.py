2b766e8f20246310ded59ec5a77b70b4822dd5e9
khubak <103487011+khubak@users.noreply.github.com>
2023-02-06 11:51:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 import pandas as pd
-import numpy as np
-import plotly.express as px
-import os
 import sqlalchemy
-import psycopg2
-from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
-#todo: parser then db creation
+#todo: parser then table creation
 
 excel = pd.read_excel(r'SpaceNK_2.0.xlsx', sheet_name=0)
 print(excel)
@@ -19,47 +14,7 @@ excel.reset_index(drop=True, inplace=True)
 
 print(excel.to_string())
 
-# connect to db
-pgconn = psycopg2.connect(
-    host='localhost',
-    user='postgres',
-    password='admin',
-    database='ExcelToPostgre')
-
-# cursor
-pgcursor = pgconn.cursor()
-pgconn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-
-# drop table
-pgcursor.execute('DROP TABLE IF EXISTS stores')
-
-# create table
-pgcursor.execute("""
-
-CREATE TABLE IF NOT EXISTS stores
-(
-    [Store No]               VAR_CHAR
-  , Store             VARCHAR(50) NOT NULL
-  , [TY Units]           VARCHAR(20) NOT NULL
-  , [LY Units]        FLOAT
-  , [TW Sales]   BIGINT
-  , [LW Sales]   BIGINT
-  , [LW Var %]   BIGINT
-  , [LY Sales]   BIGINT
-  , [LY Var %]   BIGINT
-  , [YTD Sales]   BIGINT
-  , [LYTD Sales]   BIGINT
-  , [LYTD Var %]   BIGINT
-);
-
-""")
-
-# commit
-pgconn.commit()
-
-# close
-pgconn.close()
-
-engine = sqlalchemy.create_engine('postgresql+psycopg2://postgres:admin@localhost/')
-excel.to_sql('tablename', engine, if_exists='replace', index=False)
-#close the connection
+engine = sqlalchemy.create_engine('postgresql+psycopg2://postgres:admin@localhost/ExcelToPostgre')
+print(engine)
+print(sqlalchemy.inspect(engine).has_table("Stores"))
+excel.to_sql('Stores', engine, if_exists='replace', index=False)
